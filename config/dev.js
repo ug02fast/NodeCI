c5fa41fd147d2b0633a1dd51998c3b07ef6662d4
Arthur Zhuk <arthurzhuk@gmail.com>
2018-05-25 12:05:37 -0700

    ci change

--- config/dev.js
+++ config/dev.js
@@ -4,5 +4,5 @@ module.exports = {
   googleClientSecret: 'KnH-rZC23z4fr2CN4ISK4srN',
   mongoURI: 'mongodb://arthur:pellegrino@ds027318.mlab.com:27318/blogdev',
   cookieKey: '123123123',
-  redisUrl: 'redis://127.0.0.1.6379',
+  redisUrl: 'redis://127.0.0.1.6379'
 };
